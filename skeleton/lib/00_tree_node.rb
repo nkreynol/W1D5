a4877f132014ddcc67bf0d63e615af8ef7ca71a0
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-02-08 13:55:45 -0500

    Add skeleton to tree node class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,38 @@
 class PolyTreeNode
+    def initialize(value) 
 
+    end
+
+    def parent
+
+    end
+
+    def children 
+
+    end
+
+    def value
+
+    end
+
+    def parent=
+
+    end
+
+    def add_child(child_node)
+
+    end
+
+    def remove_child(child_node)
+
+    end
+
+    def dfs(target_value)
+
+    end
+
+    def bfs(target_value)
+
+    end
+    
 end
